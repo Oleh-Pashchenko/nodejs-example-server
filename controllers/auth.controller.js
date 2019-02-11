00cf2d623780f76ebacdc4ab59e87dfc38ce55e5
Oleh <oleh.pashchenko@gmail.com>
2019-02-12 00:30:23 +0200

    Updated auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,35 @@
-const { verify } = require('jsonwebtoken');
-const { INTERNAL_SERVER_ERROR, NOT_ACCEPTABLE } = require('http-status-codes');
-
-const { getTokenAsync } = require('../services/redis.service');
+const {
+    INTERNAL_SERVER_ERROR,
+    NOT_ACCEPTABLE,
+} = require('http-status-codes');
+const {
+    verify,
+} = require('jsonwebtoken');
+const {
+    getTokenAsync,
+} = require('../services/redis.service');
 
 module.exports = async (req, res, next) => {
     try {
         if (!req.headers.authorization) return res.status(NOT_ACCEPTABLE).send('Need authorization token in header');
 
         const token = req.headers.authorization.replace('Bearer ', '');
-        const verified = verify(token, process.env.SESSION_SECRET);
-        const redisToken = getTokenAsync(verified);
 
-        if (verified === redisToken) {
+        const verified = verify(token);
+        const redisToken = await getTokenAsync(verified.id);
+
+        const redisVerified = verify(redisToken);
+
+        if (verified.id === redisVerified.id) {
             return next();
         }
 
-        return res.status(NOT_ACCEPTABLE).json({ message: 'Session not found' });
+        return res.status(NOT_ACCEPTABLE).json({
+            message: 'Session not found',
+        });
     } catch (error) {
-        return res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
+        return res.status(INTERNAL_SERVER_ERROR).json({
+            message: error.message,
+        });
     }
 };
